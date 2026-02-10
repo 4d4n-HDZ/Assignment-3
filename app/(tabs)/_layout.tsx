bf2647751358f7156ed410e61e96dd016d4c1e9c
Tanner Petreman <tanner.petreman@edu.sait.ca>
2026-02-10 16:03:06 -0700

    worked on the navigation tabs again...

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,23 @@
-import { Stack } from "expo-router";
-import Tabs from "expo-router";
+import { Tabs } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+export default function NavigationBar() {
+  return (
+    <Tabs
+    screenOptions={{
+      headerShown: false,
+      tabBarShowLabel: false,
+    }}>
+      <Tabs.Screen name="index"/>
+      <Tabs.Screen name="feed"
+      options={{
+        tabBarIcon: ({color, size }) => (
+          <MaterialIcons name="home" size={size} color={color} />
+        )
+      }}/>
+      <Tabs.Screen name="search"/>
+      <Tabs.Screen name="shorts"/>
+    </Tabs>
+  )
 }
+
